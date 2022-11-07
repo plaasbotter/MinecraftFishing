3c9ebab08bc80537f03c3e75e7e91ece4d8e0be9
plaasbotter <nwalters27@gmail.com>
2022-11-07 07:27:29 +0200

    Add project files.

---
+++ Imaging.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace MinecraftFishing
+{
+    public class Imaging
+    {
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        public static extern bool ShowWindow(IntPtr handle, int nCmdShow);
+        [System.Runtime.InteropServices.DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        private static Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public static IntPtr GetProcess(string name)
+        {
+            Process[] processes = Process.GetProcessesByName(name);
+            if (processes.Length > 0)
+            {
+                return processes[0].MainWindowHandle;
+            }
+            else
+            {
+                return new IntPtr();
+            }
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        public static void BringToFront(IntPtr pTemp)
+        {
+            ShowWindow(pTemp, 9);
+        }
+        public static Bitmap CaptureApplication(IntPtr hwnd)
+        {
+            var rect = new RECT();
+            GetWindowRect(hwnd, out rect);
+
+            int width = rect.Right - rect.Left;
+            int height = rect.Bottom - rect.Top;
+
+            var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            using (Graphics graphics = Graphics.FromImage(bmp))
+            {
+                graphics.CopyFromScreen(rect.Left, rect.Top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+            }
+
+            return bmp;
+        }
+        public static List<Point> LocateFoodStamps(Bitmap img)
+        {
+            List<Point> points = new List<Point>();
+
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    var col = img.GetPixel(x, y);
+                    if (col.R == 223 && col.G == 177 && col.B == 143)
+                    {
+                        points.Add(new Point(x, y));
+                    }
+                }
+            }
+            return points;
+        }
+        public static Color InvestigateFoodStamps(Bitmap img,Point point)
+        {
+            Color col = Color.White;
+            col = img.GetPixel(point.X, point.Y);
+            return col;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MinecraftFishing
+{
+    class Program
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0008;
+        private const int MOUSEEVENTF_LEFTUP = 0x0010;
+        public static bool firstScan = true;
+        public static List<Point> globalFoodStamps = null;
+        static void Main(string[] args)
+        {
+            Task.Run(() => GameLoop(0,0));
+
+            Console.WriteLine("Press any key to close...");
+            Console.Read();
+        }
+
+        private static void GameLoop(int attempts, int caught)
+        {
+            Process mainProp = null;
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (process.ProcessName.Contains("Java") || process.ProcessName.Contains("java"))
+                {
+                    if (process.MainWindowTitle.Contains("Minecraft"))
+                    {
+                        mainProp = process;
+                    }
+                }
+            }
+            if (mainProp != null)
+            {
+                Imaging.BringToFront(mainProp.MainWindowHandle);
+                if (firstScan)
+                {
+                    Thread.Sleep(5000);
+                    globalFoodStamps = new List<Point>();
+                    using (Bitmap screenshot = Imaging.CaptureApplication(mainProp.MainWindowHandle))
+                    {
+                        var ListOfPossibleFoodStamps = Imaging.LocateFoodStamps(screenshot);
+                        int maxY = int.MinValue;
+                        int currentX = 0;
+                        int currentXAdd1 = 0;
+                        foreach(var point in ListOfPossibleFoodStamps)
+                        {
+                            if (point.Y >  maxY)
+                            {
+                                maxY = point.Y;
+                            }
+                            if (point.X +1 != currentXAdd1)
+                            {
+                                if (point.X > currentXAdd1)
+                                {
+                                    currentX = point.X;
+                                    currentXAdd1 = currentX + 1;
+                                    globalFoodStamps.Add(new Point(currentXAdd1, maxY));
+                                }
+                            }
+                        }
+                    }
+                    firstScan = false;
+                }
+                else if (!firstScan)
+                {
+                    using (Bitmap screenshot = Imaging.CaptureApplication(mainProp.MainWindowHandle))
+                    {
+                        var newNikes = Imaging.InvestigateFoodStamps(screenshot,globalFoodStamps[globalFoodStamps.Count-1]);
+                        Console.WriteLine("*Reading*");
+                        if (newNikes.R == 45 && newNikes.G == 35 && newNikes.B == 29)
+                        {
+                            Console.WriteLine("Fish");
+                            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                            Thread.Sleep(100);
+                            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                            Thread.Sleep(2500);
+                            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                            Thread.Sleep(100);
+                            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                            caught++;
+                            Console.WriteLine("STATS:");
+                            Console.WriteLine("Caught: " + caught);
+                            Console.WriteLine("Attempts: " + (attempts/2));
+                        }
+                        attempts++; 
+                    }
+                }
+            }
+            Thread.Sleep(480);
+            GameLoop(attempts,caught);
+        }
+    }
+}
